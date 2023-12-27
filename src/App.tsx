25755536b08acbf75262e5437a2c32c4763540fe
Ceylin <caltepeceylin@gmail.com>
2023-12-27 18:13:31 +0300

    feat: create work list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
-import WorkCard from './component/work-card/WorkCard';
+
+import ListWorkPage from './page/list-work/ListWorkPage';
 
 function App() {
   return (
     <>
-      <WorkCard work={{name:'Deneme', description:'asddsasdsd', amount:'18', start:'12/12/2023', finish:"13/12/2023"}}/>
+      <ListWorkPage/>
     </>
   );
 }
