89182850416e797c6308a8cccfcd10dc74ef18c2
Ceylin <caltepeceylin@gmail.com>
2023-12-28 11:17:26 +0300

    feat: available work list page and delete freelancer list page

---
+++ src/page/list-available-work/ListAvailableWorkPage.tsx
@@ -0,0 +1,70 @@
+import { Box, Container, Divider, Link, List, ListItem, ListItemAvatar, ListItemText, Pagination, Typography } from '@mui/material';
+import { Work } from '../../model/Work';
+import { useState } from 'react';
+import WorkHistoryTwoToneIcon from '@mui/icons-material/WorkHistoryTwoTone';
+import React from 'react';
+
+function ListAvailableWorkPage() {
+
+    const listCount = 10;
+    const works: Work[] = [{ name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    { name: "asd", amount: "10", description: "description", start: "start", finish: "finish" },
+    ];
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    return (
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            {
+                works.map((work, index) => {
+                    return (
+                        <Link underline='none'>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <WorkHistoryTwoToneIcon sx={{color: "primary.dark", fontSize: "3rem"}}/>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={work.name}
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography
+                                                variant="body2"
+                                                color="text.secondary"
+                                            >
+                                                {work.description}
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                color="primary"
+                                            >
+                                                {work.start} - {work.finish}
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                color="primary.light"
+                                            >
+                                                $ {work.amount}
+                                            </Typography>
+                                            
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                            <Divider variant="inset" component="li" />
+                        </Link>
+                    );
+                })
+            }
+        </List>
+    );
+}
+
+export default ListAvailableWorkPage;
